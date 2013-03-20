bc69fa8430dbfc45a983ff00309e87a4ca75be40
Ethan-G <gillespie.ethan@gmail.com>
2013-03-20 21:57:38 +1000

    Added Dummy Login and Password activities with basic field checking. Main activity (we should rename that...) redirects to login at start.

--- ContactaniserApp/src/com/example/contactaniserapp/MainActivity.java
+++ ContactaniserApp/src/com/example/contactaniserapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.contactaniserapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,17 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+//		if (!credentials_stored) {
+			 startActivity(new Intent(this, LoginActivity.class));
+//		}
+		
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// populate project list/s here
 	}
 
 	@Override
@@ -17,6 +30,21 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	        case R.id.menu_change_password:
+	        	openPasswordActivity();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	public void openPasswordActivity() {
+		startActivity(new Intent(this, ChangePassword.class));
 	}
 
 }
