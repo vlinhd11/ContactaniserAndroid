d9ea7537368c6be4f69dfbf5cce5b7cba0343f99
Ethan-G <gillespie.ethan@gmail.com>
2013-03-21 14:39:20 +1000

    added encryption library and built partial (commented-out) code to use it.

---
+++ ContactaniserApp/src/com/example/contactaniserapp/ChangePassword.java
@@ -0,0 +1,99 @@
+package com.example.contactaniserapp;
+
+import org.jasypt.util.password.StrongPasswordEncryptor;
+
+import com.example.contactaniserapp.InfoDialog;
+import com.example.contactaniserapp.R;
+import android.os.Build;
+import android.os.Bundle;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class ChangePassword extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_change_password);
+		findViewById(R.id.txtOldPwd).requestFocus();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.change_password, menu);
+		return true;
+	}
+	
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	public void changePassword(View view) {
+		EditText txtOldPwd = (EditText) findViewById(R.id.txtOldPwd);
+		EditText txtNewPwd = (EditText) findViewById(R.id.txtNewPwd);
+		EditText txtConfPwd = (EditText) findViewById(R.id.txtConfirmPwd);
+		
+		txtOldPwd.setError(null);
+		txtNewPwd.setError(null);
+		txtConfPwd.setError(null);
+		View focusView = null;
+		boolean cancel = false;
+		
+		
+		if (txtConfPwd.getText().toString().equals("")) {
+			txtConfPwd.setError(getText(R.string.error_field_required));
+			cancel = true;
+			focusView = txtConfPwd;
+		}
+		if (txtNewPwd.getText().toString().equals("")) {
+			txtNewPwd.setError(getText(R.string.error_field_required));
+			cancel = true;
+			focusView = txtNewPwd;
+		}
+		if (txtOldPwd.getText().toString().equals("")) {
+			txtOldPwd.setError(getText(R.string.error_field_required));
+			cancel = true;
+			focusView = txtOldPwd;
+		}
+		
+		if (!cancel) {
+			if (!txtNewPwd.getText().toString().equals(txtConfPwd.getText().toString())) {
+				txtConfPwd.setError(getText(R.string.password_no_match));
+				cancel = true;
+				focusView = txtConfPwd;
+			} else if (txtNewPwd.getText().toString().equals(txtOldPwd.getText().toString())) {
+				txtNewPwd.setError(getText(R.string.password_not_new));
+				cancel = true;
+				focusView = txtNewPwd;
+			}
+		}
+		
+		if (cancel) {
+			focusView.requestFocus();
+			return;
+		}
+		
+//		StrongPasswordEncryptor encryptor = new StrongPasswordEncryptor();
+//		String oldPwdEncrypted = encryptor.encryptPassword(txtOldPwd.getText().toString());
+//		String newPwdEncrypted = encryptor.encryptPassword(txtNewPwd.getText().toString());
+//		
+//		// TODO: some function to retrieve encrypted password? or do that at start
+//		String serverPwd = SOMETHING;
+//		
+//		if (!serverPwd.equals(oldPwdEncrypted)) {
+//			txtOldPwd.setError(getText(R.string.error_incorrect_password));
+//			txtOldPwd.requestFocus();
+//			return;
+//		}
+		
+		// TODO: some function to send off new hashed password
+		// error check here
+
+		InfoDialog feedback = new InfoDialog();
+		feedback.dialogMessage = "Password sucessfully changed.";
+		feedback.show(getFragmentManager(), WINDOW_SERVICE);
+//		startActivity(new Intent(this, MainActivity.class));
+	}
+
+}
---
+++ ContactaniserApp/src/com/example/contactaniserapp/InfoDialog.java
@@ -0,0 +1,30 @@
+package com.example.contactaniserapp;
+
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+//said it needed that ^^ but it really shouldn't - using wrong version of something? There's no way we need the latest API just for a dialog box.
+public class InfoDialog extends DialogFragment {
+	
+	String dialogMessage;
+	
+	@Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(dialogMessage)
+               .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                       // nothing, this class is just for easy display of feedback.
+                   }
+               });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
