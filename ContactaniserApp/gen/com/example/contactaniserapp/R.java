cf27a22ef569b9a5cae377495867589b6a142791
walterlam <walterlam217@gmail.com>
2013-04-11 12:57:08 +1000

    Connection Package attached

--- ContactaniserApp/gen/com/example/contactaniserapp/R.java
+++ ContactaniserApp/gen/com/example/contactaniserapp/R.java
@@ -24,33 +24,44 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f05000e;
+        public static final int action_settings=0x7f050015;
         public static final int btnChange=0x7f050003;
-        public static final int login=0x7f05000a;
-        public static final int login_form=0x7f050007;
-        public static final int login_status=0x7f050005;
-        public static final int login_status_message=0x7f050006;
-        public static final int menu_change_password=0x7f05000d;
-        public static final int menu_logoff=0x7f05000c;
+        public static final int button1=0x7f050010;
+        public static final int button2=0x7f050011;
+        public static final int button3=0x7f050012;
+        public static final int json=0x7f050005;
+        public static final int log=0x7f050006;
+        public static final int login=0x7f05000e;
+        public static final int login_form=0x7f05000b;
+        public static final int login_status=0x7f050009;
+        public static final int login_status_message=0x7f05000a;
+        public static final int menu_change_password=0x7f050014;
+        public static final int menu_logoff=0x7f050013;
         public static final int pager=0x7f050000;
-        public static final int password=0x7f050009;
-        public static final int sign_in_button=0x7f05000b;
+        public static final int password=0x7f05000d;
+        public static final int project=0x7f050007;
+        public static final int sign_in_button=0x7f05000f;
+        public static final int task=0x7f050008;
         public static final int txtConfirmPwd=0x7f050004;
         public static final int txtNewPwd=0x7f050002;
         public static final int txtOldPwd=0x7f050001;
-        public static final int username=0x7f050008;
+        public static final int username=0x7f05000c;
     }
     public static final class layout {
         public static final int activity_change_password=0x7f030000;
-        public static final int activity_login=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_task_list=0x7f030003;
+        public static final int activity_down_sync=0x7f030001;
+        public static final int activity_login=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_sync=0x7f030004;
+        public static final int activity_task_list=0x7f030005;
     }
     public static final class menu {
         public static final int activity_main=0x7f080000;
         public static final int change_password=0x7f080001;
-        public static final int login=0x7f080002;
-        public static final int task_list=0x7f080003;
+        public static final int down_sync=0x7f080002;
+        public static final int login=0x7f080003;
+        public static final int sync=0x7f080004;
+        public static final int task_list=0x7f080005;
     }
     public static final class string {
         public static final int action_forgot_password=0x7f060010;
