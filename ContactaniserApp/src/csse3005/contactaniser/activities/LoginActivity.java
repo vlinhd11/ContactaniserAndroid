d5caf4ab7e5889acb6f5690fe43417c47f350d0c
guntels <ryan.dan.anderson@gmail.com>
2013-04-18 15:38:54 +1000

    Changed Launcher icons.

--- ContactaniserApp/src/csse3005/contactaniser/activities/LoginActivity.java
+++ ContactaniserApp/src/csse3005/contactaniser/activities/LoginActivity.java
@@ -34,7 +34,6 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 import csse3005.contactaniserapp.R;
 
 /**
