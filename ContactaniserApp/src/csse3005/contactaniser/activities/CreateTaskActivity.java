665fce28950723e166faf9367fe0f6baf56fe146
guntels <ryan.dan.anderson@gmail.com>
2013-05-10 02:23:04 +1000

    Added date picker and category to create task layout.

--- ContactaniserApp/src/csse3005/contactaniser/activities/CreateTaskActivity.java
+++ ContactaniserApp/src/csse3005/contactaniser/activities/CreateTaskActivity.java
@@ -5,12 +5,32 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import java.util.Calendar;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+
 public class CreateTaskActivity extends Activity {
 
+
+	private Button btnChangeDate;
+ 
+	private int year;
+	private int month;
+	private int day;
+ 
+	static final int DATE_DIALOG_ID = 999;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_task);
+		
+		setCurrentDateOnView();
+		addListenerOnButton();
 	}
 
 	@Override
@@ -19,5 +39,60 @@ public class CreateTaskActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_create_task, menu);
 		return true;
 	}
+	
+	// display current date
+		public void setCurrentDateOnView() {
+			
+			final Calendar c = Calendar.getInstance();
+			year = c.get(Calendar.YEAR);
+			month = c.get(Calendar.MONTH);
+			day = c.get(Calendar.DAY_OF_MONTH);
+			Button p1_button = (Button)findViewById(R.id.btnChangeDate);
+			p1_button.setText(Integer.toString(day)
+					+ "/" + Integer.toString(month) + "/" + 
+					Integer.toString(year));
+	 
+		}
+	 
+		public void addListenerOnButton() {
+	 
+			btnChangeDate = (Button) findViewById(R.id.btnChangeDate);
+	 
+			btnChangeDate.setOnClickListener(new OnClickListener() {
+	 
+				@Override
+				public void onClick(View v) {
+	 
+					showDialog(DATE_DIALOG_ID);
+	 
+				}
+	 
+			});
+	 
+		}
+	 
+		@Override
+		protected Dialog onCreateDialog(int id) {
+			switch (id) {
+			case DATE_DIALOG_ID:
+			   // set date picker as current date
+			   return new DatePickerDialog(this, datePickerListener, 
+	                         year, month,day);
+			}
+			return null;
+		}
+	 
+		private DatePickerDialog.OnDateSetListener datePickerListener 
+	                = new DatePickerDialog.OnDateSetListener() {
+	 
+			// when dialog box is closed, below method will be called.
+			public void onDateSet(DatePicker view, int selectedYear,
+					int selectedMonth, int selectedDay) {
+				year = selectedYear;
+				month = selectedMonth;
+				day = selectedDay;
+	 
+			}
+		};
 
 }
